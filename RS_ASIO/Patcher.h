963e13644a45928e9258457d48b94effb05c6da0
Micael Dias <oss@micaeldias.com>
2022-09-18 02:46:36 +0100

    Support patching both new and old versions of the game

--- RS_ASIO/Patcher.h
+++ RS_ASIO/Patcher.h
@@ -1,3 +1,8 @@
 #pragma once
 
 void PatchOriginalCode();
+
+std::vector<void*> FindBytesOffsets(const BYTE* bytes, size_t numBytes);
+void Patch_CallAbsoluteIndirectAddress(const std::vector<void*>& offsets, void* TargetFn);
+void Patch_CallRelativeAddress(const std::vector<void*>& offsets, void* TargetFn);
+void Patch_ReplaceWithNops(void* offset, size_t numBytes);
