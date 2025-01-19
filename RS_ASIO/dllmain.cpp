c8d6cff4a16ffcaa88591afdc659abb5a034736d
Micael Dias <oss@micaeldias.com>
2025-01-19 23:23:59 +0000

    Bump version

--- RS_ASIO/dllmain.cpp
+++ RS_ASIO/dllmain.cpp
@@ -19,7 +19,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 		DisableThreadLibraryCalls(hModule);
 
 		rslog::InitLog();
-		rslog::info_ts() << " - Wrapper DLL loaded (v0.7.3d)" << std::endl;
+		rslog::info_ts() << " - Wrapper DLL loaded (v0.7.4)" << std::endl;
 		InitPatcher();
 		PatchOriginalCode();
 		break;
